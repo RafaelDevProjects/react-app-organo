fc3e2c89beeeda6df11ecb04e78476cbfbdeb6ca
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2024-03-17 21:46:15 -0300

    arrumando o projeto

---
+++ organo/src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner(){
+    //JSX
+    return(
+        <header className = "banner">
+            <img src = "/img/banner.png" alt = "banner principal"/>
+        </header>
+        
+    )
+}
+
+export default Banner
